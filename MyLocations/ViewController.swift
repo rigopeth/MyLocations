0529c30b58e0b73260f7237c686c5846cc66d726
rigo.dominguezg@gmail.com <rigo.dominguezg@gmail.com>
2022-11-11 14:50:44 -0600

    Initial Commit

---
+++ MyLocations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyLocations
+//
+//  Created by Rigoberto Dominguez Garcia on 11/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
