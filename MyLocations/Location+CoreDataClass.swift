56846ce9d719a7d45329a0807905f6de4d626c30
rigopeth <rigo.dominguezg@gmail.com>
2023-02-07 12:37:16 -0600

    Create Location Core Data Files

---
+++ MyLocations/Location+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Location+CoreDataClass.swift
+//  MyLocations
+//
+//  Created by Rigoberto Dominguez on 06/02/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Location)
+public class Location: NSManagedObject {
+
+}
