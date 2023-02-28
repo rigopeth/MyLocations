de958c417dd4a5e3c56c32ffd2c1f2f5dec73069
rigopeth <rigo.dominguezg@gmail.com>
2023-02-27 21:21:07 -0600

    Changes in LocationDetailsViewController and LocationsViewController

--- MyLocations/LocationDetailsViewController.swift
+++ MyLocations/LocationDetailsViewController.swift
@@ -46,11 +46,23 @@ class LocationDetailsViewController: UITableViewController {
     @IBOutlet var longitudeLabel: UILabel!
     @IBOutlet var addressLabel: UILabel!
     @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var addPhotoLabel: UILabel!
+    @IBOutlet var imageHeight: NSLayoutConstraint!
     
     var coordinate = CLLocationCoordinate2D(latitude: 0, longitude: 0)
     var placemark: CLPlacemark?
     
     var categoryName = "No Category"
+    
+    var image : UIImage?
+    
+    var observer : Any!
+    
+    deinit {
+        print("*** deinit \(self)")
+        NotificationCenter.default.removeObserver(observer!)
+    }
     
     
     // MARK: - Actions
@@ -71,6 +83,7 @@ class LocationDetailsViewController: UITableViewController {
             //1
             
             location = Location(context: managedObjectContext)
+            location.photoID = nil
         }
         
         //2
@@ -83,6 +96,25 @@ class LocationDetailsViewController: UITableViewController {
         location.placemark = placemark
         
         //3
+        
+        //Save Image
+        
+        if let image = image {
+            //1
+            
+            if !location.hasPhoto {
+                location.photoID = Location.nextPhotoID() as NSNumber
+            }
+            
+            //2
+            if let data = image.jpegData(compressionQuality: 0.5) {
+                do {
+                    try data.write(to: location.photoURL, options: .atomic)
+                } catch {
+                    print("Error writing file: \(error)")
+                }
+            }
+        }
         
         do{
             try managedObjectContext.save()
@@ -110,8 +142,17 @@ class LocationDetailsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        listenForBackgroundNotification()
+        
         if let location = locationToEdit{
             title = "Edit Location"
+            
+            if location.hasPhoto {
+                if let theImage = location.photoImage {
+                    show(image: theImage)
+                }
+            }
+            
         }
         
         descriptionTextView.text = descriptionText
@@ -191,6 +232,11 @@ class LocationDetailsViewController: UITableViewController {
         didSelectRowAt indexPath: IndexPath) {
             if indexPath.section == 0 && indexPath.row == 0 {
                 descriptionTextView.becomeFirstResponder()
+            } else if indexPath.section == 1 && indexPath.row == 0 {
+                //takePhotoWithCamera()
+                //choosePhotoFromLibrary()
+                tableView.deselectRow(at: indexPath, animated: true)
+                pickPhoto()
             }
     }
     
@@ -206,6 +252,103 @@ class LocationDetailsViewController: UITableViewController {
             }
             descriptionTextView.resignFirstResponder()
         }
+    
+    func show(image : UIImage) {
+        imageView.image = image
+        imageView.isHidden = false
+        addPhotoLabel.text = ""
+        imageHeight.constant = 260
+        tableView.reloadData()
+    }
+    
+    func listenForBackgroundNotification(){
+        observer = NotificationCenter.default.addObserver(forName: UIScene.didEnterBackgroundNotification, object: nil, queue: OperationQueue.main) { [weak self] _ in
+            
+            if let weakSelf = self {
+                if weakSelf.presentedViewController != nil{
+                    weakSelf.dismiss(animated: false, completion: nil)
+                }
+                weakSelf.descriptionTextView.resignFirstResponder()
+            }
+        }
+    }
 
+}
+
+extension LocationDetailsViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: - Image Helper Methods
+    
+    func takePhotoWithCamera() {
+        
+        // Con esto yo llamo a la camara
+        
+        let imagePicker = UIImagePickerController()
+        
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = self
+        imagePicker .allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+    func choosePhotoFromLibrary() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    // MARK: - Image Picker Delegates
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage
+        
+        if let theImage = image {
+            show(image: theImage)
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true,completion: nil)
+    }
+    
+    // MARK: Photo Extensions
+    
+    func pickPhoto () {
+        if true || UIImagePickerController.isSourceTypeAvailable(.camera){
+            showPhotoMenu()
+        } else {
+            choosePhotoFromLibrary()
+        }
+    }
+    
+    func showPhotoMenu() {
+        let alert = UIAlertController (title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let actCancel = UIAlertAction(title: "Cancel", style: .cancel,handler: nil)
+        
+        alert.addAction(actCancel)
+        
+        let actPhoto = UIAlertAction(title: "Take Photo", style: .default) {
+            _ in
+            self.takePhotoWithCamera()
+        }
+        
+        alert.addAction(actPhoto)
+        
+        let actLibrary = UIAlertAction(title: "Choose From Library", style: .default) {
+            _ in
+            self.choosePhotoFromLibrary()
+        }
+        
+        alert.addAction(actLibrary)
+        
+        present(alert, animated: true, completion: nil)
+    }
     
 }
--- MyLocations/LocationsViewController.swift
+++ MyLocations/LocationsViewController.swift
@@ -72,6 +72,7 @@ class LocationsViewController : UITableViewController {
     ){
         if editingStyle == .delete {
             let location = fetchedResultsController.object(at: indexPath)
+            location.removePhotoFile()
             managedObjectContext.delete(location)
             do {
                 try managedObjectContext.save()
